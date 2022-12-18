2ededc7ebd5800c8286a60f080eef2a36b47e280
Andrija Cajic <35900499+andrijasniffer@users.noreply.github.com>
2022-12-18 15:34:07 +0100

    IT255-DZ11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {PreporukeComponent} from "./preporuke/preporuke.component";
 import {PonudaComponent} from "./ponuda/ponuda.component";
 import {ONamaComponent} from "./o-nama/o-nama.component";
 
 const routes: Routes = [
-  { path: "", component: PonudaComponent },
-  { path: "preporuke", component: PreporukeComponent },
-  { path: "onama", component: ONamaComponent }
+  {path: "", component: PonudaComponent},
+  {path: "preporuke", component: PreporukeComponent},
+  {path: "onama", component: ONamaComponent},
+  {path: "home", redirectTo: ""}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
